6a78c399b352f4eacc8577ad003a5a31fa8b74ff
Christian Froehlingsdorf <christian.froehlingsdorf@rewe-digital.com>
2017-01-08 18:38:36 +0100

    derp

--- dbs/queries.sql
+++ dbs/queries.sql
@@ -1,1 +1,6 @@
-SELECT topic,  SUM(score) AS score FROM tweets WHERE createdAt BETWEEN '2017-01-05 09:30:00.000 +00:00' AND '2017-01-05 18:00:00.000 +00:00' GROUP BY topic;
+-- get an overview of valuable tweet's scores
+SELECT topic,  SUM(score) AS score, SUM(comparative) AS comparative,
+AVG(serious) AS serious,  AVG(manualRating) AS manual FROM tweets
+WHERE createdAt BETWEEN '2017-01-08 14:00:00.000 +00:00' AND '2017-01-08 14:30:00.000 +00:00'
+AND classifierRating = 1
+GROUP BY topic;
